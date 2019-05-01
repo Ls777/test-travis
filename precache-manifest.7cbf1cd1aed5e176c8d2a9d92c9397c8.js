9b2a9ab0733fbe8fe66e80c61a47ffa108a4f41a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-01 23:15:28 +0000

    Deploy Ls777/test-travis to github.com/Ls777/test-travis.git:gh-pages

---
+++ precache-manifest.7cbf1cd1aed5e176c8d2a9d92c9397c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc0ef39ce4872c4d28a1",
+    "url": "/test-travis/static/css/main.11fc8205.chunk.css"
+  },
+  {
+    "revision": "bc0ef39ce4872c4d28a1",
+    "url": "/test-travis/static/js/main.9e687548.chunk.js"
+  },
+  {
+    "revision": "c759d09f7951d5174e8a",
+    "url": "/test-travis/static/js/runtime~main.35d826e4.js"
+  },
+  {
+    "revision": "8a8ecc960f2c4bb182ac",
+    "url": "/test-travis/static/js/2.fc8106d7.chunk.js"
+  },
+  {
+    "revision": "7531677e1130b6d2c98155ce97ef86ca",
+    "url": "/test-travis/index.html"
+  }
+];
